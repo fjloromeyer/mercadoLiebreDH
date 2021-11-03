15344292667a3d039f4cd9b0fda7968bd703166c
fjloromeyer <fjloromeyer@gmail.com>
2021-11-03 00:08:37 -0300

    agrega node_modules + modifica app.js

--- app.js
+++ app.js
@@ -14,6 +14,10 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "./views/home.html"));
 });
 
+app.post("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./views/home.html"));
+});
+
 app.get("/register", (req, res) => {
   res.sendFile(path.join(__dirname, "./views/register.html"));
 });
